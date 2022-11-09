bbb1473fdf58abf476b9cd0ca7141b470b21a212
=Pradeepragul <1606pradeepragul@gmail.com>
2022-11-09 11:09:23 +0530

    Sprint -3

---
+++ Project-Developement-Phase/sprint-3/createadmin.py
@@ -0,0 +1,27 @@
+import getpass
+from CURD import get_admin_by_username , add_admin ,connect_db
+from app import bcrypt
+
+def create_admin():
+    connect_db() #connecting to db2
+    while(1):
+        print("Creating a admin account ")
+        admin_id = input("Enter ur new Admin-ID ")
+        admin = get_admin_by_username(admin_id)
+        if admin:
+            print("Admin-Id Already Exist.")
+        else:
+            break
+
+    for i in range(3):
+        paswd = getpass.getpass("Enter password : ")
+        cpasswd = getpass.getpass("Confirm password : ")
+        if paswd == cpasswd:
+            paswd= bcrypt.generate_password_hash(paswd)
+            add_admin(admin_id,paswd)
+            break
+        else:
+            print("Password mismatch")
+
+if __name__ == "__main__":
+    create_admin()
